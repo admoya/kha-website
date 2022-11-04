7e3f69855f4569ec4ec2e9abf4ae4a1b15c5c593
Stephanie Cruz <stephanie@csma.technology>
2022-11-04 10:52:07 -0400

    feat(about-plain): about page without social media for now (#9)
    * draft layout of about page
    
    * updated names and positions

---
+++ src/lib/components/TeamMember.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  export let position: string;
+  export let name: string;
+  export let img: string;
+</script>
+
+<div class="team-member">
+  <img alt="team member" class="member-photo" src={img} />
+  <div class="member-details">
+    <p class="name">{name}</p>
+    <p class="position">{position}</p>
+  </div>
+</div>
+
+<style>
+  .team-member {
+    display: flex;
+    flex-direction: row;
+    width: 20rem;
+    margin: 20px 0px;
+  }
+
+  .member-photo {
+    width: 100px;
+    height: 100px;
+    border-radius: 100%;
+    -o-object-fit: fill;
+    object-fit: fill;
+  }
+
+  .member-details {
+    text-align: left;
+    margin-left: 10px;
+  }
+
+  .name {
+    font-family: 'roboto';
+    font-weight: 300;
+    font-size: 24px;
+    margin-bottom: 0px;
+  }
+
+  .position {
+    font-family: 'roboto';
+    font-weight: 250;
+    font-size: 18px;
+    margin-top: 0px;
+  }
+</style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,71 @@
+<script lang="ts">
+  import placeholder from '$lib/assets/avatars/avatar-placeholder.png';
+    import TeamMember from '$lib/components/TeamMember.svelte';
+</script>
+
 <head>
   <title>About - KHA</title>
 </head>
 
-<h1>About</h1>
+<head>
+  <title>About - KHA</title>
+</head>
+
+<h1 class="page-heading">About</h1>
+<section class="container">
+  <h2 class="page-subheading">Our Mission</h2>
+  <p>
+    To serve and inform the community of the Village of Kendale, Kendale Country Club Estates and Kendale South, to promote safety, foster social and  community activities, 
+    to enhance and protect our environment and to further interaction of the Association Membership  while preserving the quality of life and sense of community in our neighborhood.
+  </p>
+
+  <h2 class="page-subheading">Our Vision</h2>
+  <p>
+    We envision a community that offers an inspiring lifestyle and quality of life, and is a desirable place in which to live and enjoy the benefits of family, friends and community.
+  </p>
+</section>
+
+<section class="container">
+  <h2 class="page-subheading">Our Team</h2>
+  <div class="team">
+    <TeamMember
+      img={placeholder}
+      position="President"
+      name="Ernie Bascuas"
+    />
+    <TeamMember
+      img={placeholder}
+      position="Vice President"
+      name="Zuriel Carbo"
+    />
+    <TeamMember
+      img={placeholder}
+      position="Treasurer"
+      name="Migdy Moya"
+    />
+    <TeamMember
+      img={placeholder}
+      position="Corresponding Secretary"
+      name="Patty Gonzalez"
+    />
+    <TeamMember
+      img={placeholder}
+      position="Past President"
+      name="Nancy Lyons"
+    />
+  </div>
+  <h2 class="page-subheading">We need your help!</h2>
+  <p>There are many positions open! If you wish to volunteer, send us a message from the <a href="/contact">contact</a> page.</p>
+</section>
+
+<style> 
+  .team {
+    display: flex; 
+    flex-flow: row wrap;
+    justify-content: space-between;
+  }
+  .break {
+    flex-basis: 100%;
+    height: 0;
+  }
+</style>
