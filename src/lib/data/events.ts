a9f91f6d7a88da7e40f7b0acc8ed1828b83239f2
Adrian Moya <moyaa1022@gmail.com>
2022-11-21 22:33:41 +0000

    fix(events): fix typos

--- src/lib/data/events.ts
+++ src/lib/data/events.ts
@@ -13,7 +13,7 @@ export type Event = {
 const events: Event[] = [
   {
     name: "First Annual Holiday Golf Cart Event",
-    description: `Join your neighbors for this family friendly event.  If you have a golf cart and want to join in the parade, be at Kendale Park at 6:30 p.m. with your decorated golf cart. Parade route will be announce week of event and all are welcomed to cheer them on throughout the route.`,
+    description: `Join your neighbors for this family friendly event.  If you have a golf cart and want to join in the parade, be at Kendale Park at 6:30 p.m. with your decorated golf cart. Parade route will be announced week of event and all are welcomed to cheer them on throughout the route.`,
     date: new Date("2022-12-23T18:45:00"),
     location: "Kendale Park",
     photo: christmas,
@@ -21,8 +21,8 @@ const events: Event[] = [
   },
   {
     name: "Movie Night",
-    description: `Join us for a movie night at the park! The movie is Home Alone. Popcorn, cookies and glow  necklaces and will be provided`,
-    date: new Date("2022-12-13T19:00:00"),
+    description: `Join us for a movie night at the park! The movie is Home Alone. Popcorn, cookies and glow  necklaces will be provided`,
+    date: new Date("2022-12-10T19:00:00"),
     location: "Kendale Park",
     photo: homeAlonePoster,
     photoAlt: "The movie poster for Elf",
